8d667f9c9f66c79ffe1e838abfe0034e209c3e81
Kristine Castres <128007011+krstnCas3@users.noreply.github.com>
2023-11-26 09:47:02 +0800

    updated.py

--- t.py
+++ t.py
@@ -7,18 +7,21 @@ from tkinter import messagebox
 root = Tk()
 root.title("NoodleDoodle")
 root.geometry("1100x600")
-
 # Declare canvas as a global variable
 canvas = None
+
+# logo
+img = PhotoImage(file='pap.png')
+root.iconphoto(False, img)
 
 # -------------- variables --------------------
 
 # stroke size options
 brush_options = [5, 10, 15,]
-pencil_options = [1, 2, 3]
+pencil_options = [0.5, 1, 1.5]
 eraser_options = [3, 20, 25, 30]
 
-stroke_size = IntVar()
+stroke_size = IntVar() 
 stroke_size.set(1)
 
 stroke_color = StringVar()
@@ -30,8 +33,8 @@ currentPoint = [0, 0]
 
 # variable for text
 textValue = StringVar()
- 
- #canvas
+
+#canvas
 canvas = None
 
 canvas_actions = []
@@ -45,7 +48,7 @@ def usePencil():
     sizeList['menu'].delete(0, 'end')  
     for size in pencil_options:
         sizeList['menu'].add_command(label=str(size), command=lambda s=size: stroke_size.set(s))
-    stroke_size.set(1)
+    stroke_size.set(0.5)
 
 def useBrush():
     global canvas
@@ -75,30 +78,12 @@ def selectColor():
 def paint(event):
     global prevPoint
     global currentPoint
-    global canvas_layers
-    global current_layer_index
     x = event.x
     y = event.y
     currentPoint = [x, y]
 
     if prevPoint != [0, 0]:
         canvas.create_line(prevPoint[0], prevPoint[1], currentPoint[0], currentPoint[1],fill=stroke_color.get(), width=stroke_size.get())
-
-    prevPoint = currentPoint
-
-    if event.type == "5":
-        prevPoint = [0, 0]
-
-def paintRight(event):
-    global prevPoint
-    global currentPoint
-    x = event.x
-    y = event.y
-    currentPoint = [x, y]
-
-    if prevPoint != [0, 0]:
-        canvas.create_polygon(prevPoint[0], prevPoint[1], currentPoint[0], currentPoint[1],fill=stroke_color.get(), outline=stroke_color.get(), width=stroke_size.get())
-        save_canvas_action()
 
     prevPoint = currentPoint
 
@@ -114,9 +99,7 @@ def clearImage():
                 canvas.create_line(action["points"], fill=action["fill"], width=action["width"])
             elif action["type"] == "polygon":
                 canvas.create_polygon(action["points"], fill=action["fill"], outline=action["outline"], width=action["width"])
-        current_action_index += 1
-    
-
+        current_action_index == 1
 
 
 def save_canvas_action():
@@ -132,12 +115,12 @@ def save_canvas_action():
 
 def saveImage():
     try:
-        fileLocation = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png"), ("JPG files", "*.jpg")])
+        fileLocation = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", ".png"), ("JPG files", ".jpg")])
         if fileLocation:
             x = root.winfo_rootx() + canvas.winfo_x()
             y = root.winfo_rooty() + canvas.winfo_y() + 100
-            x1 = x + canvas.winfo_width()
-            y1 = y + canvas.winfo_height()
+            x1 = x + canvas.winfo_width() + 300
+            y1 = y + canvas.winfo_height() + 100 
             img = ImageGrab.grab(bbox=(x, y, x1, y1))
             img.save(fileLocation)
             showImage = messagebox.askyesno("NoodleDoodle", "Do you want to open the saved image?")
@@ -145,10 +128,7 @@ def saveImage():
                 import os
                 os.startfile(fileLocation)  
     except Exception as e:
-        messagebox.showinfo("NoodleDoodle", f"Error occurred: {e}")
-
-
-# ------------------- User Interface -------------------
+        messagebox.showinfo("NoodleDoodle", "Error occurred")
 
 # Frame - 1 : Tools
 
@@ -180,7 +160,7 @@ strokeSize.grid(row=0, column=0)
 
 # colorBoxFrame
 
-colorBoxFrame = Frame(frame1, height=100, width=100, relief=SUNKEN, borderwidth=3)
+colorBoxFrame = Frame(frame1, height=100, width=100, relief=SUNKEN, borderwidt=3)
 colorBoxFrame.grid(row=0, column=2)
 
 colorBoxButton = Button(colorBoxFrame, text="Select Color", width=10, command=selectColor)
@@ -237,11 +217,10 @@ frame2.grid(row=1, column=0)
 # Create the canvas globally
 canvas = Canvas(frame2, height=500, width=1100, bg="white")
 canvas.grid(row=0, column=0)
+
+#Bind
 canvas.bind("<B1-Motion>", paint)
 canvas.bind("<ButtonRelease-1>", paint)
-canvas.bind("<B3-Motion>", paintRight)
-
-
 
 root.resizable(False, False)
 root.mainloop()
